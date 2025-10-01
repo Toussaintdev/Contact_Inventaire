d7db9773e36f4018c1d2f59b87b45569a97b9a70
Toussaintdev <toussainttcham01@gmail.com>
2025-10-01 14:52:59 +0000

    ajout du required aux input, script pour creer la base de données et inserer les données à partir du formulaire

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == "POST":
+        donnees = request.form
+        contact = donnees.get('contact')
+        inventaire = donnees.get('inventaire')
+        insert = (contact, inventaire)
+        conn = sqlite3.connect('gestion_contact.db')
+        cur = conn.cursor()
+        sql = "INSERT INTO info (contact, inventaire) VALUES (?, ?)"
+        cur.execute(sql, insert)
+        conn.commit
+        cur.close()
+        conn.close()
+        return render_template('index.html')
+    else:
+        conn = sqlite3.connect('gestion_contact.db')
+        cur = conn.cursor()
+        cur.execute('''CREATE TABLE IF NOT EXISTS info(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    contact TEXT,
+                    inventaire TEXT)
+                    ''')
+        cur.close()
+        conn.close()
+        return render_template('index.html')
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
